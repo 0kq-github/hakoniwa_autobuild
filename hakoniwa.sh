84acd40475303d1d7b625e13d3abcad3833561fd
0kq <61573675+0kq-github@users.noreply.github.com>
2021-05-09 15:28:49 +0900

    Update hakoniwa.sh

--- hakoniwa.sh
+++ hakoniwa.sh
@@ -9,15 +9,28 @@ if [ $USER = root ]; then
  rm -rf hakoniwa_autobuild
  mkdir hakoniwa_autobuild
  cd hakoniwa_autobuild
+ java -version &> /dev/null
+  if [ $? -ne 0 ] ; then
+   yum install -y java-1.8.0-openjdk-headless
+   apt install -y openjdk-8-jre-headless
+  else
+   :
+ fi
+ unzip -hh &> /dev/null
+  if [ $? -ne 0 ] ; then
+   yum install -y unzip
+   apt install -y unzip
+  else
+   :
+ fi
  wget -O hakoniwa.zip https://cdn.discordapp.com/attachments/832626889335504986/840799450346684446/ver2.0_.zip
- yum install -y unzip
- apt install -y unzip
- unzip hakoniwa.zip
  wget -O server.jar https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar
+ unzip hakoniwa.zip
  echo java -Xmx$mem -Xms$mem -server -jar server.jar nogui >> start.sh
  chmod +x start.sh
  echo "eula=true" >> eula.txt
- ./start.sh
+ echo "魔導の箱庭を起動中..."
+ bash ./start.sh
 else
  echo "権限が必要です"
  echo "sudo ./hakoniwa.shで実行してね"
