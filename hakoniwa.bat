bdd7fbdf19dbbc061e452c72fdf0eeab0ce7444b
0kq-github <shotaki2002@gmail.com>
2021-05-31 07:47:38 +0900

    修正

--- hakoniwa.bat
+++ hakoniwa.bat
@@ -23,7 +23,7 @@ curl -L -o hakoniwa.zip https://github.com/wancomatter/mado-hakoniwa/releases/do
 curl -L -o server.jar https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar
 call powershell -command "Expand-Archive -Path '.\hakoniwa.zip' -DestinationPath '.\'"
 echo @echo off>start.bat
-echo title –‚“±‚Ì” ’ëver2.1 - 1.16.5
+echo title –‚“±‚Ì” ’ëver2.1 - 1.16.5>>start.bat
 echo java -Xmx%mem% -Xms%mem% -server -jar server.jar nogui>>start.bat
 echo pause>> start.bat
 echo eula=true>eula.txt
