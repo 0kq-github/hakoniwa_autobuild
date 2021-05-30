9c3c652a19073047d939d4e49a6e5a97af4a1304
0kq <61573675+0kq-github@users.noreply.github.com>
2021-05-31 07:43:46 +0900

    2.1に対応

--- hakoniwa.sh
+++ hakoniwa.sh
@@ -33,7 +33,7 @@ if [ $USER = root ]; then
   else
    :
  fi
- wget -O hakoniwa.zip https://cdn.discordapp.com/attachments/832626889335504986/845493409829093416/ver2.0.2.zip
+ wget -O hakoniwa.zip https://github.com/wancomatter/mado-hakoniwa/releases/download/2.1/ver2.1.zip
  wget -O server.jar https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar
  unzip hakoniwa.zip
  echo java -Xmx$mem -Xms$mem -server -jar server.jar nogui >> start.sh
